11f96fda6e71f4c601f58de9afdde34e0af566ad
Kanro-Code <tervelde.thijs@gmail.com>
2024-07-28 23:38:33 +0200

    Popout shopping list

--- GuildBankStocker.lua
+++ GuildBankStocker.lua
@@ -4,7 +4,7 @@ SLASH_STOCKER2 = "/guildbankstocker"
 local SHOPPINGLIST = {
 	-- tab 1
 	["Potion of the Tol'vir"] = 20 * 21,
-	["Volcanic Potion"] = 20 * 21,
+	["Volcanic Potion"] = 20 * 28,
 	["Golemblood Potion"] = 20 * 14,
 	["Mythical Mana Potion"] = 20 * 14,
 	["Earthen Potion"] = 20 * 7,
@@ -23,15 +23,22 @@ local SHOPPINGLIST = {
 	["Flask of the Draconic Mind"] = 3 * 28
 }
 
+local FRAME_NAME = "Shoppinglist"
+
 local WAITCOUNT = 0;
+local f1 = CreateFrame("Frame")
+f1:RegisterEvent("PLAYER_LOGIN")
+f1:SetScript("OnEvent", function(self, event)
+	close_chatframe()
+end)
 
 SlashCmdList["STOCKER"] = function(_msg)
 	WAITCOUNT = 0;
 	poll_gbank()
 
-	local f = CreateFrame("Frame")
-	f:RegisterEvent("GUILDBANKBAGSLOTS_CHANGED")
-	f:SetScript("OnEvent", function(self, event)
+	local f2 = CreateFrame("Frame")
+	f2:RegisterEvent("GUILDBANKBAGSLOTS_CHANGED")
+	f2:SetScript("OnEvent", function(self, event)
 		WAITCOUNT = WAITCOUNT - 1
 		if (WAITCOUNT == 0) then
 			print_difference(scan_gbank())
@@ -39,34 +46,35 @@ SlashCmdList["STOCKER"] = function(_msg)
 	end)
 end
 
-function print_difference(current)
-	local string = {}
-	for name, demand in pairs(SHOPPINGLIST) do
-		local buycount = 0
+function print_difference(storage)
+	local string = { "Buy the following:" }
+	for demandName, demantCount in pairs(SHOPPINGLIST) do
+		local missing = 0
 		local link
-		if current[name] then
-			buycount = demand - current[name][2]
-			link = current[name][1]
+		if storage[demandName] then
+			missing = demantCount - storage[demandName][2]
+			link = storage[demandName][1]
 		else
-			buycount = demand
-			link = GetItemInfo(name)[2]
+			missing = demantCount
+			link = GetItemInfo(demandName)[2]
 			if link == nil then
-				link = name
+				link = demandName
 			end
 		end
 
-		if buycount > 0 then
-			string[#string+1] = format("%s - %d", link, buycount)
+		if missing > 0 then
+			string[#string + 1] = format("%s - %d", link, missing)
 		end
 	end
 
-	if #string == 0 then
+	if #string == 1 then
 		print("Nothing to buy")
 		return
-	else 
-		print("Buy the following:")
-		for i = 1, #string do
-			print(string[i])
+	else
+		local chatframe = FCF_OpenNewWindow(FRAME_NAME)
+		for i = 0, #string do
+			-- print(string[i])
+			chatframe:AddMessage(string[i])
 		end
 	end
 end
@@ -82,12 +90,12 @@ end
 
 function scan_gbank()
 	local contents = {}
-	for tab=1,3 do
-		for i=1,98 do 
-			local link = GetGuildBankItemLink(tab,i)
+	for tab = 1, 3 do
+		for i = 1, 98 do
+			local link = GetGuildBankItemLink(tab, i)
 			if link then
 				local name = GetItemInfo(link)
-				local _, count = GetGuildBankItemInfo(tab,i)
+				local _, count = GetGuildBankItemInfo(tab, i)
 				if contents[name] then
 					contents[name][2] = contents[name][2] + count
 				else
@@ -97,4 +105,15 @@ function scan_gbank()
 		end
 	end
 	return contents
+end
+
+function close_chatframe()
+	print("CLOSING CHAT FRAME", NUM_CHAT_WINDOWS)
+	for i = 1, NUM_CHAT_WINDOWS do
+		local name = FCF_GetChatWindowInfo(i)
+		if (name == FRAME_NAME) then
+			FCF_Close(_G['ChatFrame' .. i])
+		end
+		-- match name then use FCF_Close(_G['ChatFrame' .. i])
+	end
 end
